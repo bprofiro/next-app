357216d5c720144c27ca3290ab6a52c686aecb8d
Brenda Profiro <brendaprofiro@gmail.com>
2020-11-01 12:29:22 -0300

    chore: create dynamic routes and static routes

---
+++ src/pages/catalog/products/[slug].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router';
+
+export default function Product() {
+  const router = useRouter();
+  return (
+    <div>
+      <h1>
+        {router.query.slug}
+      </h1>
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Title } from '../styles/pages/Home';
+
+export default function Home() {
+  return (
+    <div>
+      <Title>
+        Hello World
+      </Title>
+    </div>
+  )
+}
